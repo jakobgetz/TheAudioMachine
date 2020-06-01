e23d2baadf36fd86bdb9cb1dc28368546e45eeb8
HowToTellAStory <jg123@hdm-stuttgart.de>
2020-06-01 21:50:42 +0200

    -Adding the Base for a Preset Browser and also trying to make it work. It does not at all for now... -Adding Velocity Handling (You can now here Trigger with lower velocity with lower Volume. However you can not change the Trigger Velocity in the UI) -Adding Pitch Handling (Same functionality as for Velocity) -Try to load the Presets "VelocityShowcase" or "PitchShowcase" to try the new features

--- the-audio-mashine/src/Components/Sequencer/PresetBrowser.js
+++ the-audio-mashine/src/Components/Sequencer/PresetBrowser.js
@@ -1,11 +1,42 @@
 import React, {Component} from 'react';
 
 class PresetBrowser extends Component {
+
+    state = {
+        presets: ["Preset 1", "Preset 2", "Preset 3", "Preset 4"]
+    }
+
+    /*
+    componentDidMount() {
+        fetch(__dirname)
+            .then(response => console.log(response))
+        console.log(__dirname + 'Presets')
+        let fs = require('browserify-fs')
+        fs.readdir(__dirname, (content, err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(content)
+            }
+        })
+    }
+    */
+
+    choosePreset = (e) => {
+        console.log(e.target.value)
+    }
+
     render() {
         return (
             <div className="PresetBrowser">
-                <label>Load your Presets here:</label>
-                <input type='file' onChange={(e) => this.props.loadPreset(e)}/>
+                <ul className="PresetDropdown">
+                    <label> ^ </label>
+                    {
+                        this.state.presets.map((preset, i) =>
+                            <li key={preset} onClick={(e) => this.choosePreset(e)}>{preset}</li>
+                        )
+                    }
+                </ul>
             </div>
         );
     }
