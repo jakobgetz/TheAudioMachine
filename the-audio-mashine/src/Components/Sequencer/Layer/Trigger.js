409d61d021cf1bae6b59813d3eb5b4e8823961bc
HowToTellAStory <jg123@hdm-stuttgart.de>
2020-06-10 16:22:52 +0200

    rounded the sequencer :))))

--- the-audio-mashine/src/Components/Sequencer/Layer/Trigger.js
+++ the-audio-mashine/src/Components/Sequencer/Layer/Trigger.js
@@ -3,13 +3,15 @@ import React, {Component} from 'react';
 class Trigger extends Component {
 
     getStyle = () => {
-        return this.props.checked ? this.props.style : this.props.style
+        return this.props.checked ? this.props.styleChecked : this.props.styleUnchecked
 
     }
 
     render() {
         return (
-            <span className={this.getStyle()} onClick={() => this.props.setTrigger(this)}>
+            <span className={this.getStyle()}>
+                <span onClick={() => this.props.setTrigger(this)}>
+                </span>
             </span>
         );
     }
