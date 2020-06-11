fc8c08d3c4cff188b539a0aeb3beb1e8647c8a3c
HowToTellAStory <jg123@hdm-stuttgart.de>
2020-06-11 13:27:49 +0200

    Added Version Info

--- the-audio-mashine/src/App.js
+++ the-audio-mashine/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                {/*<Header/>*/}
+                <Header/>
                 <Sequencer/>
                 {/*<Library/>*/}
             </div>
--- the-audio-mashine/src/Components/Header.js
+++ the-audio-mashine/src/Components/Header.js
@@ -5,6 +5,11 @@ class Header extends Component {
         return (
             <div className="Header">
                 <h1>The Audio Machine</h1>
+                Version Info:<br/>
+                    - Saving Presets not working yet<br/>
+                    - Preset + / - not working yet<br/>
+                    - Press the grey Button in the middle of the sequencer to start<br/>
+                    - Playhead always starts at the very top of the sequencer<br/>
             </div>
         );
     }
