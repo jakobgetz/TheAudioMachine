130efb2b9d8d5e00f3030e0593a556577ce15d2b
P.Esch <jeschers@googlemail.com>
2020-06-20 23:12:29 +0200

    added .htaccess for apache server

--- the-audio-mashine/src/App.js
+++ the-audio-mashine/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Sequencer from "./Components/Sequencer/Sequencer";
 import Header from "./Components/Header";
 import "./CSS/mainStyles.scss"
@@ -8,15 +8,15 @@ import Contact from "./Components/Contact/Contact";
 import {Route} from 'react-router-dom'
 
 function App() {
-        return (
-            <div>
-                <Header/>
-                <BurgerMenu/>
-                    <Route exact path='/'><Sequencer/></Route>
-                    <Route exact path='/about'><About/></Route>
-                    <Route exact path='/contact'><Contact/></Route>
-            </div>
-        );
+    return (
+        <div>
+            <Header/>
+            <BurgerMenu/>
+            <Route exact path='/'><Sequencer/></Route>
+            <Route exact path='/about'><About/></Route>
+            <Route exact path='/contact'><Contact/></Route>
+        </div>
+    );
 }
 
 export default App;
