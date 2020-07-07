072172028981eeb7b5215e52502097b6773ab9a4
P.Esch <jeschers@googlemail.com>
2020-07-07 20:18:02 +0200

    implemented umuts css styles

--- the-audio-mashine/src/App.js
+++ the-audio-mashine/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./Components/Header";
 import "./CSS/mainStyles.scss"
+import "./CSS2/main.scss"
 import BurgerMenu from "./Components/BurgerMenu";
 
 function App() {
