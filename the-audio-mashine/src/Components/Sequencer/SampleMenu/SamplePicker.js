13f856e6f2a87fecea50639fc32c7773e88df1c6
Jakob Getz <jg123@hdm-stuttgart.de>
2020-07-10 14:19:19 +0200

    Added Umuts Styles. Design is not responsive so it may be that it looks kinda wierd dependend of your screen resolution

--- the-audio-mashine/src/Components/Sequencer/SampleMenu/SamplePicker.js
+++ the-audio-mashine/src/Components/Sequencer/SampleMenu/SamplePicker.js
@@ -2,33 +2,48 @@ import React, {Component} from 'react';
 
 class SamplePicker extends Component {
 
+    symbols = ["fas fa-shoe-prints", "fas fa-shoe-prints", "fas fa-shoe-prints", "fas fa-shoe-prints", "fas fa-shoe-prints", "fas fa-shoe-prints"]
+
     getStyle = layerId => {
-        return this.props.isShown ? "sampleButtonC" + layerId : "sampleButton" + layerId
+        return this.props.isShown ? "sidenav__button-selected-" + layerId : "sidenav__button-" + layerId
+    }
+
+    getMuteStyle = layerId => {
+        if (this.props.isShown) {
+            return this.props.layer.isMute ? "extra active-selected-" + layerId + " sidenav__button-mute-selected" : "extra sidenav__extra-" + layerId + " sidenav__button-mute"
+        } else {
+            return this.props.layer.isMute ? "extra inactive-selected-" + layerId + " sidenav__button-mute-selected" : "extra sidenav__extra-" + layerId + " sidenav__button-mute"
+        }
+    }
+
+    getSoloStyle = layerId => {
+        if (this.props.isShown) {
+            return this.props.layer.isSolo ? "extra active-selected-" + layerId + " sidenav__button-solo-selected" : "extra sidenav__extra-" + layerId + " sidenav__button-solo"
+        } else {
+            return this.props.layer.isSolo ? "extra inactive-selected-" + layerId + " sidenav__button-solo-selected" : "extra sidenav__extra-" + layerId + " sidenav__button-solo"
+        }
     }
 
     render() {
         const {layer, openPicker, loadSample, isShown, setLayerGain, setLayerPan, setLayerMute, setLayerSolo} = this.props
 
         return (
-            <div className={"sidenav__button-" + layer.layerId + " sidenav__box"}>
-                <div className={this.getStyle(layer.layerId + 1)} onClick={() => openPicker(layer.layerId)}>
-                    {layer.name}
-                    <div className={"symbol"}></div>
+            <div className={this.getStyle(layer.layerId)}>
+                <div className={"sidenav__name"}>{layer.name}</div>
+                <div className={"symbol sidenav__symbol-" + layer.layerId} onClick={() => openPicker(layer.layerId)}>
+                    <i className={this.symbols[layer.layerId]}></i>
                 </div>
+
+                <div className="sidenav__box">
+                    <div className={this.getMuteStyle(layer.layerId)} onClick={() => setLayerMute(layer.layerId)}>M</div>
+
+                    <div className={this.getSoloStyle(layer.layerId)} onClick={() => setLayerSolo(layer.layerId)}>S</div>
+                </div>
+
                 {isShown ?
                     <ul className="sampleFeatures">
                         <li>
                             <input type="file" onChange={(e) => loadSample(e, layer.layerId)}/>
-                        </li>
-                        <li>
-                            <input type="button"
-                                   onClick={() => setLayerSolo(layer.layerId)}
-                                   value="Solo"/>
-                        </li>
-                        <li>
-                            <input type="button"
-                                   onClick={() => setLayerMute(layer.layerId)}
-                                   value="Mute"/>
                         </li>
                         <li>Gain
                             <input type="range" value={layer.layerGain}
