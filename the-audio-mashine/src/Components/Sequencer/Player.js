36bdd196ce0799c0a0ae9200da778232e4a3ad21
Jakob Getz <jg123@hdm-stuttgart.de>
2020-07-14 19:36:21 +0200

    test 2

--- the-audio-mashine/src/Components/Sequencer/Player.js
+++ the-audio-mashine/src/Components/Sequencer/Player.js
@@ -214,7 +214,7 @@ class Player extends Component {
 
     /**
      * Sets master volume
-     * @param volume is the parameter for the output volum
+     * @param volume is the parameter for the output volume
      */
     setVolume = (volume) => {
         this.masterGain.gain.setValueAtTime(volume / 100, this.ctx.currentTime)
