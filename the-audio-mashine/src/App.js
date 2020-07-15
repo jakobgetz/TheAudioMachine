ba0179164e4d7013e46bb6ce7e5f25172bf18d75
Jakob Getz <jg123@hdm-stuttgart.de>
2020-07-15 11:14:31 +0200

    excluded Burger Menu for now

--- the-audio-mashine/src/App.js
+++ the-audio-mashine/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import Header from "./Components/Header"
+import BurgerMenu from "./Components/BurgerMenu"
+import Sequencer from "./Components/Sequencer/Sequencer";
 import "./CSS/mainStyles.scss"
 import "./CSS2/main.scss"
 import "./CSS2/fontawesome-free-5.13.0-web/css/all.min.css"
-import BurgerMenu from "./Components/BurgerMenu"
 
 function App() {
     return (
         <div>
             {/*<Header/>*/}
-            <BurgerMenu/>
+            {/*<BurgerMenu/>*/}
+            <Sequencer/>
         </div>
     );
 }
