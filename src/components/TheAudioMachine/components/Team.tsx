77303cb622d8e3a3aa6d76dcbf207ec138211975
P.Esch <jeschers@googlemail.com>
2021-01-16 17:18:33 +0100

    added new team photo and links

--- src/components/TheAudioMachine/components/Team.tsx
+++ src/components/TheAudioMachine/components/Team.tsx
@@ -4,126 +4,129 @@ import {useDispatch} from "react-redux";
 import {toggleAbout} from "../../../redux";
 
 import {
-  CLOSE_TEAM,
-  TEAM,
-  TEAM_CONTENT,
-  TEAM_DEPARTMENT,
-  TEAM_HEADING,
-  TEAM_IMAGE,
-  TEAM_IMAGE_CROPPER,
-  TEAM_LINK,
-  TEAM_NAME,
-  TEAM_TEXT,
+    CLOSE_TEAM,
+    TEAM,
+    TEAM_CONTENT,
+    TEAM_DEPARTMENT,
+    TEAM_HEADING,
+    TEAM_IMAGE,
+    TEAM_IMAGE_CROPPER,
+    TEAM_LINK,
+    TEAM_NAME,
+    TEAM_TEXT,
 } from "../styled";
 
 export const Team = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  return (
-    <TEAM>
-      <CLOSE_TEAM onClick={() => dispatch(toggleAbout())} />
-      <TEAM_CONTENT>
-        <TEAM_HEADING>TEAM</TEAM_HEADING>
-        <TEAM_IMAGE_CROPPER>
-          <TEAM_IMAGE
-            src="/assets/images/team/pascal_escher_portrait.jpg"
-            alt="Pascal Escher"
-            push={0}
-          />
-        </TEAM_IMAGE_CROPPER>
-        <TEAM_TEXT>
-          <TEAM_NAME>Pascal Escher</TEAM_NAME>
-          <TEAM_DEPARTMENT>Coding</TEAM_DEPARTMENT>
-          <TEAM_LINK>
-            <a href={"https://www.xing.com/profile/Pascal_Escher"}
-               title="Xing-Profil">
-              <i className="fab fa-xing"/>
-            </a>
-            <a href={"https://handicraft-band.de/"} title="Handicraft-Band">
-              <i className="fas fa-music"/>
-            </a>
-          </TEAM_LINK>
-        </TEAM_TEXT>
-        <TEAM_IMAGE_CROPPER>
-          <TEAM_IMAGE
-              src="/assets/images/team/joris_kaufmann_portrait.jpg"
-              alt="Joris Kaufmann"
-              push={0}
-          />
-        </TEAM_IMAGE_CROPPER>
-        <TEAM_TEXT>
-          <TEAM_NAME>Joris Kaufmann</TEAM_NAME>
-          <TEAM_DEPARTMENT>Coding</TEAM_DEPARTMENT>
-        </TEAM_TEXT>
-        <TEAM_IMAGE_CROPPER>
-          <TEAM_IMAGE
-            src="/assets/images/team/steffen_marquardt_portrait.jpg"
-            alt="Steffen Marquardt"
-            push={4}
-          />
-        </TEAM_IMAGE_CROPPER>
-        <TEAM_TEXT>
-          <TEAM_NAME>Steffen Marquardt</TEAM_NAME>
-          <TEAM_DEPARTMENT>Coding</TEAM_DEPARTMENT>
-          <TEAM_LINK>
-            <a href={"http://thesample.life/"}>
-              <i className="fas fa-headphones" />
-            </a>
-            <a href={"https://www.instagram.com/thesamplelifeofficial/"}>
-              <i className="fab fa-instagram" />
-            </a>
-          </TEAM_LINK>
-        </TEAM_TEXT>
-        <TEAM_IMAGE_CROPPER>
-          <TEAM_IMAGE
-              src="/assets/images/team/jakob_getz_portrait.jpg"
-              alt="Jakob Getz"
-              push={1}
-          />
-        </TEAM_IMAGE_CROPPER>
-        <TEAM_TEXT>
-          <TEAM_NAME>Jakob Getz</TEAM_NAME>
-          <TEAM_DEPARTMENT>Coding</TEAM_DEPARTMENT>
-          <TEAM_LINK>
-            <a href={"https://soundcloud.com/howtotellastory"}>
-              <i className="fas fa-headphones" />
-            </a>
-            <a href={"https://de.linkedin.com/in/jakob-getz-8a8082178/en-us"}>
-              <i className="fab fa-linkedin" />
-            </a>
-          </TEAM_LINK>
-        </TEAM_TEXT>
-        <TEAM_IMAGE_CROPPER>
-          <TEAM_IMAGE
-            src="/assets/images/team/unbekannt_portrait.jpg"
-            alt="Umut Cocak"
-            push={0}
-          />
-        </TEAM_IMAGE_CROPPER>
-        <TEAM_TEXT>
-          <TEAM_NAME>Umut Cocak</TEAM_NAME>
-          <TEAM_DEPARTMENT>UI</TEAM_DEPARTMENT>
-          <TEAM_LINK>
-            <a href={"http://thesample.life/"}>
-              <i className="fas fa-headphones" />
-            </a>
-            <a href={"https://www.instagram.com/thesamplelifeofficial/"}>
-              <i className="fab fa-instagram" />
-            </a>
-          </TEAM_LINK>
-        </TEAM_TEXT>
-        <TEAM_IMAGE_CROPPER>
-          <TEAM_IMAGE
-            src="/assets/images/team/unbekannt_portrait.jpg"
-            alt="Benedikt Ludwig"
-            push={0}
-          />
-        </TEAM_IMAGE_CROPPER>
-        <TEAM_TEXT>
-          <TEAM_NAME>Benedikt Ludwig</TEAM_NAME>
-          <TEAM_DEPARTMENT>Sounddesign</TEAM_DEPARTMENT>
-        </TEAM_TEXT>
-      </TEAM_CONTENT>
-    </TEAM>
-  );
+    return (
+        <TEAM>
+            <CLOSE_TEAM onClick={() => dispatch(toggleAbout())}/>
+            <TEAM_CONTENT>
+                <TEAM_HEADING>TEAM</TEAM_HEADING>
+                <TEAM_IMAGE_CROPPER>
+                    <TEAM_IMAGE
+                        src="/assets/images/team/pascal_escher_portrait.jpg"
+                        alt="Pascal Escher"
+                        push={0}
+                    />
+                </TEAM_IMAGE_CROPPER>
+                <TEAM_TEXT>
+                    <TEAM_NAME>Pascal Escher</TEAM_NAME>
+                    <TEAM_DEPARTMENT>Coding</TEAM_DEPARTMENT>
+                    <TEAM_LINK>
+                        <a href={"https://www.xing.com/profile/Pascal_Escher"}
+                           title="Xing-Profil">
+                            <i className="fab fa-xing"/>
+                        </a>
+                        <a href={"https://handicraft-band.de/"} title="Handicraft-Band">
+                            <i className="fas fa-music"/>
+                        </a>
+                    </TEAM_LINK>
+                </TEAM_TEXT>
+                <TEAM_IMAGE_CROPPER>
+                    <TEAM_IMAGE
+                        src="/assets/images/team/joris_kaufmann_portrait.jpg"
+                        alt="Joris Kaufmann"
+                        push={0}
+                    />
+                </TEAM_IMAGE_CROPPER>
+                <TEAM_TEXT>
+                    <TEAM_NAME>Joris Kaufmann</TEAM_NAME>
+                    <TEAM_DEPARTMENT>Coding</TEAM_DEPARTMENT>
+                </TEAM_TEXT>
+                <TEAM_IMAGE_CROPPER>
+                    <TEAM_IMAGE
+                        src="/assets/images/team/steffen_marquardt_portrait.jpg"
+                        alt="Steffen Marquardt"
+                        push={4}
+                    />
+                </TEAM_IMAGE_CROPPER>
+                <TEAM_TEXT>
+                    <TEAM_NAME>Steffen Marquardt</TEAM_NAME>
+                    <TEAM_DEPARTMENT>Coding</TEAM_DEPARTMENT>
+                    <TEAM_LINK>
+                        <a href={"https://www.linkedin.com/in/steffen-marquardt-2a460a166/"}>
+                            <i className="fab fa-linkedin"/>
+                        </a>
+                        <a href={"https://open.spotify.com/artist/3HSImMBGEJ031Rl2ZRNmTF"}>
+                            <i className="fab fa-spotify"/>
+                        </a>
+                        <a href={"https://www.instagram.com/evenbeats/"}>
+                            <i className="fab fa-instagram"/>
+                        </a>
+                    </TEAM_LINK>
+                </TEAM_TEXT>
+                <TEAM_IMAGE_CROPPER>
+                    <TEAM_IMAGE
+                        src="/assets/images/team/jakob_getz_portrait.jpg"
+                        alt="Jakob Getz"
+                        push={0.75}
+                    />
+                </TEAM_IMAGE_CROPPER>
+                <TEAM_TEXT>
+                    <TEAM_NAME>Jakob Getz</TEAM_NAME>
+                    <TEAM_DEPARTMENT>Coding</TEAM_DEPARTMENT>
+                    <TEAM_LINK>
+                        <a href={"https://de.linkedin.com/in/jakob-getz-8a8082178/en-us"}>
+                            <i className="fab fa-linkedin"/>
+                        </a>
+                        <a href={"https://soundcloud.com/howtotellastory"}>
+                            <i className="fas fa-headphones"/>
+                        </a>
+                    </TEAM_LINK>
+                </TEAM_TEXT>
+                <TEAM_IMAGE_CROPPER>
+                    <TEAM_IMAGE
+                        src="/assets/images/team/unbekannt_portrait.jpg"
+                        alt="Umut Cocak"
+                        push={0}
+                    />
+                </TEAM_IMAGE_CROPPER>
+                <TEAM_TEXT>
+                    <TEAM_NAME>Umut Cocak</TEAM_NAME>
+                    <TEAM_DEPARTMENT>UI</TEAM_DEPARTMENT>
+                    <TEAM_LINK>
+                        <a href={"http://thesample.life/"}>
+                            <i className="fas fa-headphones"/>
+                        </a>
+                        <a href={"https://www.instagram.com/thesamplelifeofficial/"}>
+                            <i className="fab fa-instagram"/>
+                        </a>
+                    </TEAM_LINK>
+                </TEAM_TEXT>
+                <TEAM_IMAGE_CROPPER>
+                    <TEAM_IMAGE
+                        src="/assets/images/team/benedikt_ludwig_portrait.jpg"
+                        alt="Benedikt Ludwig"
+                        push={0.5}
+                    />
+                </TEAM_IMAGE_CROPPER>
+                <TEAM_TEXT>
+                    <TEAM_NAME>Benedikt Ludwig</TEAM_NAME>
+                    <TEAM_DEPARTMENT>Sounddesign</TEAM_DEPARTMENT>
+                </TEAM_TEXT>
+            </TEAM_CONTENT>
+        </TEAM>
+    );
 };
